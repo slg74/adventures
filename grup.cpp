fa875e163f432f80b43dd51ff2c2361f4d9cd3a4
U-MSI\scott <scott.layne.gillespie@gmail.com>
2017-04-29 19:39:36 -0500

    comments

--- grup.cpp
+++ grup.cpp
@@ -31,10 +31,13 @@ int main(int argc, char **argv) {
         lines.push_back(text_line);                      // this builds our words-lines vector.
     }
 
-    std::string converted_pattern = convert_regex(pattern); 
+    // convert the regex to a pcre acceptable regular expression pattern. 
+    std::string converted_pattern = convert_regex(pattern);  
 
+    // build our vector of matching lines.
     matching_lines = find_matching_lines(lines, converted_pattern);
 
+    // display each matched linie in our vector. 
     for (int i = 0; i < matching_lines.size(); i++) {
 	    std::cout << matching_lines[i] << std::endl;
     }
